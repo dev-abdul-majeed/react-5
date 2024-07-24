40cb13e7f19f36433b44e63a59fd84057f944fca
majeedahmed9379 <41028620+majeedahmed9379@users.noreply.github.com>
2024-07-24 15:36:25 +0500

    Revert "Added a new Card for Home page (#1)"
    This reverts commit 3d810c43b0208f32419d2fe5d3ad78a469349230.

--- src/components/CardNew.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const CardNew = () => {
-  return (
-    <div>CardNew</div>
-  )
-}
-
-export default CardNew
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NavBar = () => {
+  return (
+    <>
+      Hello World
+    </>
+  )
+}
+
+export default NavBar
